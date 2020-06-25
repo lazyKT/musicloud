917820d802c7aa82d02ea889f05b306368cc78b3
lazyKT <kyaw.thitlwin.me@gmail.com>
2020-06-25 22:23:58 +0800

    Home Page Changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,68 @@ import Cookies from 'js-cookie'
 import { BrowserRouter as Router, Link, withRouter } from 'react-router-dom';
 import '../App.css';
 
+/**
+ * Styling for Home DOM Elments
+ */
+const styles = {
+  form: {
+    border: "gray solid 0.2px",
+    padding: "20px",
+    borderRadius: "10px"
+  },
+  UsernameLabel: {
+    fontSize: "15px",
+    fontWeight: "800",
+  },
+  PwdLabel: {
+    display: "inline",
+    width: "fit-content",
+    fontSize: "15px",
+    fontWeight: "800",
+    margin: "10px 0px",
+    float: "left"
+  },
+  ForgetPwd: {
+    display: "inline",
+    width: "fit-content",
+    fontSize: "15px",
+    fontWeight: "800",
+    color: "coral",
+    margin: "10px 0px",
+    float: "right"
+  },
+  input: {
+    display: "block",
+    width: "100%",
+    height: "30px",
+    lineHeight: "20px",
+    fontSize: "15px",
+    padding: "5px",
+    border: "gray solid 0.2px",
+    borderRadius: "5px",
+    boxSizing: "border-box"
+  },
+  pre: {
+    display: "inline",
+    fontSize: "15px",
+    fontFamily: "sans-serif"
+  },
+  error: {
+    margin: "10px auto",
+    height: "max-content",
+    padding: "5px 15px",
+    background: "firebrick",
+    border: "0.1px solid",
+    borderRadius: "5px",
+    color: "white",
+    fontSize: "13px",
+    width: "fit-content",
+  }
+}
+
+/**
+ * Home Function. Sign In Page
+ */
 function Home(props){
   const [ user, setUser ] = useState({username:'',password:''});
   const [ loginUser, setLoginUser ] = useState(null);
@@ -55,26 +117,35 @@ function Home(props){
       })
   }
   
+  /**
+   * Rendering
+   */
   return(
     <div className="mainDiv">
       <div className="container">
         <div className="innerDiv">
-          <form onSubmit={handleSubmit}>
-              <h4 className="registerTitle">Login</h4>
-              <input value={ user.username } name="username" 
-                  placeholder="Username" onChange={handleChange} required/><br/>
-              <input value={ user.password } type="password" name="password" 
-                  placeholder="Password" onChange={handleChange} required/><br/>
-              <button type="submit">Login</button>
-              <button className="registerBtn" onClick={handleRegister}>
-                Register
-              </button>
-              <Link to='/forget-password'>Forget Password?</Link>
-              { error ? 
-                <div className="errorResponse">
-                  { error }
-                </div>:null}
+        <h4 className="registerTitle">Login</h4>
+          <form style={styles.form} onSubmit={handleSubmit}>
+              <p style={styles.UsernameLabel}>Username or email address</p>
+              <input style={styles.input} value={ user.username } name="username" 
+                onChange={handleChange} required/>
+              <p style={styles.PwdLabel}>Password</p>
+              <Link to='/forget-password' style={styles.ForgetPwd}>
+                Forget Password?
+              </Link>
+              <input style={styles.input} value={ user.password } type="password" name="password" 
+                onChange={handleChange} required/>
+              <button className="login-button" type="submit">Login</button>
+              {/* Link To register */}
+              <pre style={styles.pre}>New to MusiCloud? </pre> 
+              <Link to='/register'>
+                Create Account...
+              </Link>
           </form>
+          { error ? 
+            <div style={styles.error}>
+              { error }
+            </div>:null}
         </div>
       </div>
     </div>
