e56327e3f1c37d64396529aac47c50f56d91fee5
lazyKT <kyaw.thitlwin.me@gmail.com>
2020-09-21 14:01:30 +0800

    Logout Done.

--- src/Components/UsersReqs/Users.js
+++ src/Components/UsersReqs/Users.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 export async function logoutUser(token) {
     console.log("token", token);
     try {
-        const response = await axios.post('http://127.0.0.1:8000/logout', { headers: { "Authorization": `Bearer ${token}` }});
-        return response;
-    } catch (error) {
+        const res = await axios.post('http://127.0.0.1:8000/logout', null,
+            { headers: { "Authorization": `Bearer ${token}` }});
+        console.log(res);
+        return res;
+    } catch(error) {
+        console.log("Error Encountered",error);
         return error;
     }
 }
