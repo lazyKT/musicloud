590e1ea473d9404b5827402cf779b6b746c2e2ca
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-01-12 05:23:15 +0800

    Burger Menu

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { userContext } from "../../Contexts/userContext";
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 import { Link } from 'react-router-dom';
 import { AdminNav } from "./AdminNav";
 import { UesrNav } from "./UserNav";
@@ -18,7 +19,19 @@ export const Nav = () => {
 
 
   const Auth = useContext(userContext);
+  const navRef = useRef();
   const [type, setType] = useState("");
+  const [showBurger, setShowBurger] = useState(false);
+
+  // onClick event on humburger button 
+  function burgerClick() {
+
+    setShowBurger(prevState => !prevState);
+
+    navRef.current.classList.toggle('show');
+
+  }
+
 
   useEffect(() => {
     const tokens = Cookies.get("tokens");
@@ -28,7 +41,7 @@ export const Nav = () => {
   }, []);
 
   return (
-    <nav className="navBar">
+    <nav className="navBar nav">
       {Auth.auth ? (
         type && type === "admin" ? (
           <AdminNav />
@@ -39,10 +52,12 @@ export const Nav = () => {
             // Navigation bar when the user not log in
             <div className="nav-container">
                 <Link className="left-nav" to="/">MusiCloud</Link>
-                <button className="hamburger">
-                    <MenuIcon />
+                <button className="hamburger" id="humburger" onClick={burgerClick}>
+                    {
+                      showBurger ? <CloseIcon/> : <MenuIcon/>
+                    }
                 </button>
-                <ul className="right-nav nav-container">         
+                <ul className="right-nav nav-container" ref={navRef} id="right-nav">         
                     <Link className="nav-element" to="/"><li>Home</li></Link>
                     <Link className="nav-element" to="/contact"><li>Contact</li></Link>
                     <Link className="nav-element" to="/support"><li>Support</li></Link>
